b3a6564e70f218ff3c55045064b53151945bfefd
sammieatkins <104380965+sammieatkins@users.noreply.github.com>
2025-02-05 22:01:27 -0700

    IT WORKED AHHHH YAYAYAY

---
+++ cms/src/app/contacts/contact-detail/contact-detail.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { Contact } from '../contact.model';
+
+@Component({
+  selector: 'cms-contact-detail',
+  standalone: false,
+  
+  templateUrl: './contact-detail.component.html',
+  styleUrl: './contact-detail.component.css'
+})
+export class ContactDetailComponent {
+  @Input() contact: Contact;
+}
---
+++ cms/src/app/contacts/contacts.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Contact } from './contact.model';
+
+@Component({
+  selector: 'cms-contacts',
+  standalone: false,
+  templateUrl: './contacts.component.html',
+  styleUrl: './contacts.component.css'
+})
+export class ContactsComponent {
+  selectedContact: Contact;
+
+}
